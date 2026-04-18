df63dd8f3aab99394fde4c792b8fddbc18546487
kikkamfybyr <akihoy06@asagi.waseda.jp>
2026-04-18 14:53:09 +0900

    refactor: remove unused server dependencies, scripts, and testing utility files

--- test_fetch.js
+++
@@ -1,8 +0,0 @@
-import { createServer } from 'http';
-import { get } from 'http';
-
-get('http://localhost:5173', (res) => {
-  let data = '';
-  res.on('data', chunk => data += chunk);
-  res.on('end', () => console.log('HTML:', data.substring(0, 500)));
-});
